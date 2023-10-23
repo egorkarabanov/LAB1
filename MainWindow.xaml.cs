681663d176e126a078ac7efd0e72b8a69e782ec5
egorkarabanov <egor.karabanov@gmail.com>
2023-10-23 14:15:56 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace LAB1
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
