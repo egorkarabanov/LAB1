97ad0485d0a5ab86be7428f4b8dde5bbe315fffb
egorkarabanov <egor.karabanov@gmail.com>
2023-10-26 08:17:31 +0200

    1

--- Rectangle.cs
+++ Rectangle.cs
@@ -15,13 +15,25 @@ namespace LAB1 {
         }
 
         public (int, int) find_sides() {
-            return (Math.Abs(point_1[0]-point_2[0]), Math.Abs(point_1[1]-point_2[1]));
+            return (Math.Abs(point_1[0] - point_2[0]), Math.Abs(point_1[1] - point_2[1]));
         }
 
         public Rectangle() {
             point_1 = new int[2];
             point_2 = new int[2];
         }
-        
+
+        public Rectangle(int x1, int y1, int x2, int y2) {
+            point_1[0] = x1;
+            point_1[1] = y1;
+            point_2[0] = x2;
+            point_2[1] = y2;
+        }
+        public Rectangle(bool auto) {
+            point_1[0] = 0;
+            point_1[1] = 0;
+            point_2[0] = 10;
+            point_2[1] = 10;
+        }
     }
 }
